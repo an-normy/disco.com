f8361aaf6f6a35a4a40799cdf8ea2f8e6d93f906
an-normy <112652463+an-normy@users.noreply.github.com>
2023-02-14 16:28:26 -0300

    Delete Untitled_Js.js

--- Untitled_Js.js
+++
@@ -1,5 +0,0 @@
-function adduser() {
-    var storageusername = document.getElementById("userName").value
-    localStorage.setItem("SU",storageusername)
-    window.location = "Untitled_Room.html"
-};
